2f3804bef57e419bd7d449eba889b7ecac7254b8
Dibendu Das <ddibendu94@gmail.com>
2022-03-18 21:47:07 +0530

    Do while loop check

---
+++ dowhile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n,i=1;
+cout<<"enter the number you want to check\n";
+cin>>n;
+    do{
+        cout<<"the number is  "<<n<<endl;
+        n--;
+
+        
+    }while(n>1);
+    return 0;
+}
