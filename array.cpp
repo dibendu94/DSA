31a346c95bb210999182ba2aa05e349bba4f22ec
Dibendu Das <ddibendu94@gmail.com>
2022-03-19 21:08:15 +0530

    starting from scracth and making complex of it

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int a[19]={1,2,3,4,5,6,7,7,5,89,65,32};
+    int i;
+    cout<<"to check value of array input array index"<<endl;
+    cin>>i;
+    cout<<" the value placed in that index is "<<a[i];
+}
