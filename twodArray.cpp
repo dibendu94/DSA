b3239dadccf1ea81fa89b4d4e70c61b7b94d41bb
Dibendu Das <ddibendu94@gmail.com>
2022-03-20 23:38:48 +0530

    in making of 2 d array program

---
+++ twodArray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[4][4];
+    int x;
+    cout<<"   enter the value of the matrixes arrays";
+    
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+             cout<<a[i][j];
+        }
+        
+    }
+    for (int j = 0; j < 4; j++)
+    {
+        for (int k = 0; k < 4; k++)
+        {
+             cout<<a[i][j];
+        }
+
+        
+    }
+}
+
+    
